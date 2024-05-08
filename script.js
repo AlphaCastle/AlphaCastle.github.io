c3acd117c494b9056efeb39387c234b3775de282
Corransk <70207064+AlphaCastle@users.noreply.github.com>
2024-05-08 22:02:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 
 const images = ['Image_1', 'Image_2', 'Image_3', 'Image_4'];
 let currentIndex = 0;
+const backgroundSlider = document.getElementById('background-slider');
 
 function changeBackgroundImage() {
-    const backgroundSlider = document.getElementById('background-slider');
-    backgroundSlider.style.backgroundImage = `url('${images[currentIndex]}')`;
-    currentIndex = (currentIndex + 1) % images.length;
+    backgroundSlider.style.opacity = 0;
+    setTimeout(() => {
+        backgroundSlider.style.backgroundImage = images[currentIndex];
+        backgroundSlider.style.opacity = 1;
+        currentIndex = (currentIndex + 1) % images.length;
+    }, 500); // Change image after 0.5 seconds (500 milliseconds)
 }
 
-changeBackgroundImage(); // Initial call
-setInterval(changeBackgroundImage, 1500); // Call every 10 seconds
+setInterval(changeBackgroundImage, 5500); // Change image every 1.5 seconds (1500 milliseconds)
